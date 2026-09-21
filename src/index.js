7ab13c2c76a5aacb55a6ed435aef6ba4556a6f0f
Aman Abuelayyan <153456442+safety-crew@users.noreply.github.com>
2026-09-21 17:29:56 +0300

    Edit index.js Edit Pizza Comp styling and price tag, and Add key attribute

--- src/index.js
+++ src/index.js
@@ -77,7 +77,7 @@ function Menu() {
         </p>
         <ul className="pizzas">
           {pizzaData.map((pizza) => (
-            <Pizza pizzaObj={pizza} />
+            <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
       </>
@@ -101,12 +101,12 @@ function Menu() {
 
 function Pizza({ pizzaObj }) {
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price}</span>
+        <span>{pizzaObj.soldOut ? `Sold Out!` : pizzaObj.price}</span>
       </div>
     </li>
   );
